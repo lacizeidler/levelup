9a37df0f64b40fcde7ac26331dc7004129fff3d1
lacizeidler <laci.zeidler@gmail.com>
2022-02-03 11:52:15 -0600

    displaying a list of games

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Game(models.Model):
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    maker = models.CharField(max_length= 100)
-    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    maker = models.CharField(max_length=100)
     number_of_players = models.IntegerField()
     skill_level = models.IntegerField()
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
